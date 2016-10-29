adcf4c029b865f0d3cbf7f2f1b49f1f6e13f2bac
trunghai89 <trunghai1289@gmail.com>
2016-10-29 15:19:45 +0530

    added more scripts

---
+++ edit-host.bat
@@ -0,0 +1,2 @@
+"C:\Program Files (x86)\Notepad++\notepad++.exe" "c:\Windows\System32\Drivers\etc\hosts"
+exit
---
+++ run-tomcat-8.bat
@@ -0,0 +1,3 @@
+set CATALINA_HOME="C:\apache-tomcat\apache-tomcat-8.0.28-windows-x64\apache-tomcat-8.0.28"
+"C:\apache-tomcat\apache-tomcat-8.0.28-windows-x64\apache-tomcat-8.0.28\bin\startup.bat"
+exit
